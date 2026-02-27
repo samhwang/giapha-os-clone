c97a0f2d30cd5989574294c5cc457e9f0ba191a5
Sam Huynh <samhwang2112.dev@gmail.com>
2026-02-27 22:57:18 +1100

    test: silence i18next support notice in tests

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -14,6 +14,7 @@ export function createI18nInstance(lang: Language = defaultLanguage) {
     lng: lang,
     fallbackLng: defaultLanguage,
     interpolation: { escapeValue: false },
+    showSupportNotice: false,
   });
   return instance;
 }
