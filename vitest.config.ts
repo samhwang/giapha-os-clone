73133ef8e2972ce5a5109870efecd0b33ff7f19e
Sam Huynh <samhwang2112.dev@gmail.com>
2026-02-27 22:59:29 +1100

    test: split testing the project into 3 separate sub projects

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,20 +1,44 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      globalSetup: ['./test/globalSetup.ts'],
-      setupFiles: ['./src/test-utils/setup.ts'],
-      include: ['src/**/*.test.{ts,tsx}'],
-      fileParallelism: false,
-      coverage: {
-        provider: 'v8',
-        include: ['src/**/*.{ts,tsx}'],
-        exclude: ['src/routeTree.gen.ts', 'src/test-utils/**', 'src/**/*.test.{ts,tsx}'],
-      },
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['src/routeTree.gen.ts', 'src/test-utils/**', 'src/**/*.test.{ts,tsx}'],
     },
-  })
-);
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'ui-components',
+          include: ['src/**/**.test.tsx'],
+          exclude: ['src/routes'],
+          environment: 'jsdom',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'server',
+          include: ['src/server/functions/**.test.ts'],
+          environment: 'node',
+          globalSetup: ['./test/globalSetup.ts'],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'integration',
+          include: ['src/routes/**.test.{ts,tsx}'],
+          environment: 'jsdom',
+          globalSetup: ['./test/globalSetup.ts'],
+        },
+      },
+    ],
+    setupFiles: ['./src/test-utils/setup.ts'],
+    fileParallelism: false,
+  },
+});
