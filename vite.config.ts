d6c23042156a157e483e033d5f6ad8af63f4ab31
Sam Huynh <samhwang2112.dev@gmail.com>
2026-03-04 20:51:40 +1100

    chore: remove tailwind package and configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), tanstackStart(), viteReact()],
 });
